0db90096e2947c7c2443fbed4c91ea77b4a6d004
cstoker <stoker.cara@gmail.com>
2017-05-01 17:38:23 -0500

    day 1 simple simon

---
+++ js/simple-simon.js
@@ -0,0 +1,37 @@
+/**
+ * Created by cstoker on 5/1/17.
+ */
+
+(function() {
+
+    "use strict";
+
+
+    var shapes = ['#green', '#blue', '#purple', '#orange'];
+    var levelArray = [];
+    var player = [];
+
+
+    var generateRandom = shapes[Math.floor(Math.random() * shapes.length)];
+
+
+    function blink(el) {
+        $(el).animate({
+            opacity: 1
+        }, 500).animate ({
+            opacity: .5
+        }, 800);
+    }
+
+    blink(generateRandom);
+
+    $(".shapes").click(function(){
+        blink(this);
+        console.log(this);
+    });
+
+
+    $(".btn btn-outline").click(function(){
+    });
+
+})();
