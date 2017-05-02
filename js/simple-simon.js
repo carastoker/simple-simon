b6f5c95bf87a14c127f8f59fc2e2263c6b014a69
cstoker <stoker.cara@gmail.com>
2017-05-02 12:51:24 -0500

    added notes to simple simon exercise.

--- js/simple-simon.js
+++ js/simple-simon.js
@@ -1,35 +1,58 @@
 /**
  * Created by cstoker on 5/1/17.
  */
-
+// iffe function
 (function () {
 
-    "use strict";
+    'use strict';
 
-
+    // convert shapes div to an array using #id
     var shapes = ['#green', '#blue', '#purple', '#orange'];
-    var levelArray = [];
+    // declare var for startGame function
+    var levelArray;
+    // store player inpute
     var player = [];
 
-    var generateRandom = shapes[Math.floor(Math.random() * shapes.length)];
 
-    function blink(el) {
-        $(el).animate({
+    console.log(levelArray);
+
+    // animate shapes to blink
+    function blink(element) {
+        $(element).animate({
             opacity: 1
         }, 500).animate({
             opacity: .5
         }, 200);
     }
 
-    blink(generateRandom);
 
-    $(".shapes").click(function () {
-        blink(this);
-        // console.log(this);
-    });
+    // create empty array, push random math shapes to array, pass array up to blink for animation,
+    //  clear if count is >= to array, otherwise keep counting up by one at set speed
+    function startGame() {
 
-    // $(".fireNext").click(function () {
-    //     console.log();
-    // });
+        levelArray = [];
+
+        // randomize shapes blink in a pattern
+        levelArray.push(shapes[Math.floor(Math.random() * shapes.length)]);
+
+        var count = 0;
+        var intervalId = setInterval(function () {
+            blink(levelArray[count]);
+            if (count >= levelArray.length) {
+                clearInterval(intervalId);
+            } else {
+                count++;
+            }
+        }, 600);
+
+    }
+        // EventListener allows blink animation to fire when btn is clicked.
+        $('.shapes').click(function () {
+            blink(this);
+        });
+        // EventListener fires random math/blink animation when btn is clicked.
+        $('#newGame').click(function () {
+            startGame();
+        });
 
 })();
